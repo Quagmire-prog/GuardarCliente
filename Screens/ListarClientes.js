1a15a69b5e400e133e261cac4598abe242d1be01
Quagmire-prog <norlanfu@gmail.com>
2025-05-13 21:01:04 -0600

    arreglo de estilo del titulo lista de cliente y el boton

--- Screens/ListarClientes.js
+++ Screens/ListarClientes.js
@@ -5,8 +5,8 @@ import React, { useState } from 'react'
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import Formulario from './Formulario';
-// import Formulario from './Formulario'
+
+
 export default function ListarClientes({ navigation }) {
     const [clientes, setClientes] = useState([]);
     const guardarNuevo = (nuevo) => {
@@ -38,41 +38,42 @@ export default function ListarClientes({ navigation }) {
     return (
 
         <View style={styles.container}>
-            <Text>ListarClientes</Text>
-            <View style={styles.botonformulario}>
+            <View style={styles.header}>
+                <Text style={styles.titulo}>Lista de Clientes</Text>
                 <TouchableOpacity style={styles.tbotonf} onPress={() => navigation.navigate('Formulario', { guardarNuevo })}>
-                    <AntDesign name="adduser" size={50} color="blue" />
+                    <AntDesign name="adduser" size={30} color="green" />
                 </TouchableOpacity>
             </View>
+
             {clientes.length === 0 ? (
                 <View style={styles.card}>
                     <Text > No hay clientes registrados.</Text>
                 </View>
             ) : (
-               
-                    <ScrollView style={styles.lista}>
-                        {clientes.map((i, index) =>
-                        (
-                            <View key={index} style={styles.card}>
 
-                                <View>
-                                    <Text style={styles.label}>#<Text>{index + 1}</Text></Text>
-                                    <Text style={styles.label}>Cédula:<Text >{i.Cedula}</Text> </Text>
-                                    <Text style={styles.label}>Nombres:<Text >{i.Nombres}</Text> </Text>
-                                    <Text style={styles.label}>Apellidos:<Text >{i.Apellidos}</Text> </Text>
-                                    <Text style={styles.label}>Fecha de nacimiento:<Text > {i.FechadeNacimiento}</Text></Text>
-                                    <Text style={styles.label}>Sexo:<Text >{i.Sexo}</Text> </Text>
-                                </View>
-                                <View style={styles.botonformulario}>
-                                    <TouchableOpacity style={styles.tboton} onPress={Eliminar}>
-                                        <MaterialIcons name="delete" size={40} color="red" />
-                                    </TouchableOpacity>
-                                </View>
+                <ScrollView style={styles.lista}>
+                    {clientes.map((i, index) =>
+                    (
+                        <View key={index} style={styles.card}>
+
+                            <View>
+                                <Text style={styles.label}>#<Text>{index + 1}</Text></Text>
+                                <Text style={styles.label}>Cédula:<Text >{i.Cedula}</Text> </Text>
+                                <Text style={styles.label}>Nombres:<Text >{i.Nombres}</Text> </Text>
+                                <Text style={styles.label}>Apellidos:<Text >{i.Apellidos}</Text> </Text>
+                                <Text style={styles.label}>Fecha de nacimiento:<Text > {i.FechadeNacimiento}</Text></Text>
+                                <Text style={styles.label}>Sexo:<Text >{i.Sexo}</Text> </Text>
                             </View>
+                            <View style={styles.botonformulario}>
+                                <TouchableOpacity style={styles.tboton} onPress={Eliminar}>
+                                    <MaterialIcons name="delete" size={40} color="red" />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
 
-                        ))}
-                    </ScrollView>
-                
+                    ))}
+                </ScrollView>
+
             )}
 
 
@@ -112,10 +113,31 @@ const styles = StyleSheet.create({
 
 
     },
-    tbotonf: {
+   header: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  backgroundColor: '#d6f5d6',
+  padding: 10,
+  borderRadius: 10,
+  marginBottom: 15,
+},
+
+titulo: {
+  fontSize: 20,
+  fontWeight: 'bold',
+  color: 'green',
+},
+
+tbotonf: {
+  backgroundColor: '#ccffcc',
+  padding: 5,
+  borderRadius: 10,
+  borderWidth:1,
+  borderColor: 'green'
+}
 
 
-    },
-    
+
 
 })
